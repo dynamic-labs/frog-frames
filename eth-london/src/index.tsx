df1f8e75c09741ce9e978eba32b9a5237adf8e26
Rohith Venkatesan <rohithvenkatesan142@gmail.com>
2024-03-11 11:16:10 -0400

    eth london frame

---
+++ eth-london/src/index.tsx
@@ -0,0 +1,32 @@
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Button, Frog, TextInput } from "frog";
+// import { neynar } from 'frog/hubs'
+
+export const app = new Frog({
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+});
+
+app.use("/*", serveStatic({ root: "./public" }));
+
+let index = 0;
+app.frame("/", (c) => {
+  const { buttonValue, status } = c;
+  if (buttonValue == "prev" && index > 0) index -= 1;
+  if (buttonValue == "next" && index < 3) index += 1;
+  const imageMap: Record<number, string> = {
+    0: "https://utfs.io/f/b33f8085-58ea-458b-9129-25dcecbd1b51-34jguv.jpeg",
+    1: "https://utfs.io/f/e930778a-092d-4417-9532-ddb29e7a32a6-hjcgih.png",
+  };
+
+  return c.res({
+    image: imageMap[index],
+    imageAspectRatio: index == 0 ? "1:1" : "1.91:1",
+    intents: [
+      <Button value="prev">Previous</Button>,
+      <Button value="next">Next</Button>,
+      <Button.Link href="mailto:matt@dynamic.xyz">Questions?</Button.Link>,
+      //,status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
