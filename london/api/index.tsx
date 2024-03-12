da17a8983b0332c3a7b1833a65094442fd5b0a70
Rohith Venkatesan <rohithvenkatesan142@gmail.com>
2024-03-12 13:21:54 -0400

    cleanup

--- london/api/index.tsx
+++ london/api/index.tsx
@@ -1,15 +1,14 @@
-import { Button, Frog, TextInput } from "frog";
+import { Button, Frog } from "frog";
 import { handle } from "frog/vercel";
 
 export const app = new Frog({
   assetsPath: "/",
   basePath: "/api",
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 });
 
 let index = 0;
 const pages = 2;
+const lastPageIndex = pages - 1;
 app.frame("/", (c) => {
   const { buttonValue, status } = c;
   if (buttonValue == "prev" && index > 0) index -= 1;
@@ -17,20 +16,15 @@ app.frame("/", (c) => {
   const imageMap: Record<number, string> = {
     0: "https://utfs.io/f/b33f8085-58ea-458b-9129-25dcecbd1b51-34jguv.jpeg",
     1: "https://utfs.io/f/b83178ba-bc0f-4185-8725-34a9d1a2bbd5-dw6b7w.png",
-    /*
-    1: "https://utfs.io/f/e930778a-092d-4417-9532-ddb29e7a32a6-hjcgih.png",
-    2: "https://utfs.io/f/282418ed-8882-451f-b013-ebc68a7fd8e9-modo2.png",
-    3: "https://utfs.io/f/b17d400b-e636-450a-a619-be942a60c516-naq7j.png",
-    */
   };
 
   return c.res({
     image: imageMap[index],
-    imageAspectRatio: "1:1", //index == 0 ? "1:1" : "1.91:1",
+    imageAspectRatio: "1:1",
     intents: [
       index != 0 && <Button value="prev">Previous</Button>,
-      index != pages - 1 && <Button value="next">Next</Button>,
-      index == pages - 1 && (
+      index != lastPageIndex && <Button value="next">Next</Button>,
+      index == lastPageIndex && (
         <Button.Link href="https://docs.dynamic.xyz/introduction/welcome">
           Get started
         </Button.Link>
