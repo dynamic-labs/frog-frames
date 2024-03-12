bc188e0d0152ddc3ccd2f0ef7564b3762c35eab9
Rohith Venkatesan <rohithvenkatesan142@gmail.com>
2024-03-12 11:27:26 -0400

    add more images

--- london/api/index.tsx
+++ london/api/index.tsx
@@ -16,6 +16,8 @@ app.frame("/", (c) => {
   const imageMap: Record<number, string> = {
     0: "https://utfs.io/f/b33f8085-58ea-458b-9129-25dcecbd1b51-34jguv.jpeg",
     1: "https://utfs.io/f/e930778a-092d-4417-9532-ddb29e7a32a6-hjcgih.png",
+    2: "https://utfs.io/f/282418ed-8882-451f-b013-ebc68a7fd8e9-modo2.png",
+    3: "https://utfs.io/f/b17d400b-e636-450a-a619-be942a60c516-naq7j.png",
   };
 
   return c.res({
@@ -24,8 +26,11 @@ app.frame("/", (c) => {
     intents: [
       <Button value="prev">Previous</Button>,
       <Button value="next">Next</Button>,
-      //<Button.Link href="mailto:matt@dynamic.xyz">Questions?</Button.Link>,
-      //,status === "response" && <Button.Reset>Reset</Button.Reset>,
+      index == 3 && (
+        <Button.Link href="https://docs.dynamic.xyz/introduction/welcome">
+          Questions?
+        </Button.Link>
+      ),
     ],
   });
 });
